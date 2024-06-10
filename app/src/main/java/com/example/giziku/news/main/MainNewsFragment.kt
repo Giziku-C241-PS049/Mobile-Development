283121943a92ef83f82958807cb1b7f213e06a81
nicolaskev <nicolaskevin50@gmail.com>
2024-06-10 19:23:40 +0700

    Deskripsi perubahan yang dilakukan

---
+++ app/src/main/java/com/example/giziku/news/main/MainNewsFragment.kt
@@ -0,0 +1,35 @@
+package com.example.giziku.news.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.giziku.databinding.FragmentMainNewsBinding
+
+
+class MainNewsFragment : Fragment() {
+    //    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentMainNewsBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainNewsBinding.inflate(layoutInflater,container,false)
+//        homeViewModel =
+//            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val root = inflater.inflate(R.layout.fragment_home, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, {
+//            textView.text = it
+//        })
+//        return root
+        return binding.root
+    }
+}
